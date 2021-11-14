3d0fc113c505ac504d4f831ec83513dfc76de43e
Xun Wilson Huang <xhuang@gmail.com>
2021-11-13 21:57:55 -0800

    added search

---
+++ src/USPage/YumSearch.tsx
@@ -0,0 +1,46 @@
+import 'antd/dist/antd.css';
+
+import { Select } from 'antd';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router';
+
+import { useBayAreaLazyQuery } from '../generated/graphql';
+
+const { Option } = Select;
+
+export const SearchInput = (props: { placeholder: string; style: any }) => {
+  const [loaded, setLoaded] = useState(false);
+  const [loadData, { data }] = useBayAreaLazyQuery();
+  const history = useHistory();
+
+  const onClick = () => {
+    if (!loaded) {
+      console.log("load!!!!");
+      loadData();
+      setLoaded(true);
+    }
+  };
+
+  return (
+    <Select
+      showSearch
+      style={{ width: 200 }}
+      placeholder="Search to Select"
+      onClick={onClick}
+      onSelect={(value, option) => {
+        history.push(`/venue/${option.key}`);
+        console.log(value);
+        console.log(option.k);
+      }}
+    >
+      {data &&
+        [...data?.allVenues?.nodes!]
+          .sort((a, b) => a?.name!.localeCompare(b?.name!)!)
+          .map((v) => (
+            <Option key={v?.key!} value={v?.name!}>
+              {v?.name}
+            </Option>
+          ))}
+    </Select>
+  );
+};
