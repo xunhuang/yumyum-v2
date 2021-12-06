ce531bcead67a1c17db5e0e270bf4ceca05485a6
Xun Wilson Huang <xhuang@gmail.com>
2021-12-06 11:50:38 -0800

    gutting the util class

--- src/yummodule/YumUtil.ts
+++ src/yummodule/YumUtil.ts
@@ -1,72 +1,52 @@
-const getDistance = require('geolib').getDistance;
-const urlparse = require('url');
+// const getDistance = require('geolib').getDistance;
 
-function urlPathParts(url, position) {
-    let path = urlparse.parse(url).path;
-    var path_parts = path.split("/");
-    return path_parts[position];
-}
+// function urlPathParts(url: string, position: number) {
+//     let path = urlparse.parse(url).path;
+//     var path_parts = path.split("/");
+//     return path_parts[position];
+// }
 
-function doesTwoUrlsMatch(one, two) {
-    let onehost = urlparse.parse(normalizeUrl(one)).hostname;
-    let twohost = urlparse.parse(normalizeUrl(two)).hostname;
+// function doesTwoUrlsMatch(one: string, two: string) {
+//     let onehost = urlparse.parse(normalizeUrl(one)).hostname;
+//     let twohost = urlparse.parse(normalizeUrl(two)).hostname;
 
-    if (!onehost.startsWith("www.")) {
-        onehost = "www." + onehost;
-    }
-    if (!twohost.startsWith("www.")) {
-        twohost = "www." + twohost;
-    }
+//     if (!onehost.startsWith("www.")) {
+//         onehost = "www." + onehost;
+//     }
+//     if (!twohost.startsWith("www.")) {
+//         twohost = "www." + twohost;
+//     }
 
-    return onehost === twohost;
-}
+//     return onehost === twohost;
+// }
 
-function doesNameMatch(name1, name2) {
+function doesNameMatch(name1: string, name2: string) {
     return name1.toLowerCase().trim() === name2.toLowerCase().trim();
 }
 
-function normalizeUrl(url) {
+export function normalizeUrl(url: string) {
     if (!url.includes("http")) {
         return "http://" + url;
     }
     return url;
 }
-function snapshotToArray(snapshot) {
-    var returnArr = [];
-    snapshot.forEach(function (childSnapshot) {
-        returnArr.push(childSnapshot);
-    });
-    return returnArr;
-}
-
-function snapshotToArrayData(snapshot) {
-    var returnArr = []
-    snapshot.forEach(function (childSnapshot) {
-        returnArr.push(childSnapshot.data());
-    });
-    return returnArr;
-}
 
 // unit in Meters
-function gpsMatch(a, b, bound = 300) {
-    if (a.latitude === null || a.longitude === null || b.latitude === null || b.longitude === null) {
-        return false;
-    }
-    let d = getDistance(
-        { latitude: a.latitude, longitude: a.longitude },
-        { latitude: b.latitude, longitude: b.longitude }
-    );
-    return (d < bound);
-}
+// function gpsMatch(a, b, bound = 300) {
+//     if (a.latitude === null || a.longitude === null || b.latitude === null || b.longitude === null) {
+//         return false;
+//     }
+//     let d = getDistance(
+//         { latitude: a.latitude, longitude: a.longitude },
+//         { latitude: b.latitude, longitude: b.longitude }
+//     );
+//     return (d < bound);
+// }
 
-const YumUtil = {
-    snapshotToArray,
-    snapshotToArrayData,
-    gpsMatch,
-    normalizeUrl,
-    doesTwoUrlsMatch,
-    doesNameMatch,
-    urlPathParts,
-}
-
-exports.YumUtil = YumUtil;
+// const YumUtil = {
+//     // gpsMatch,
+//     normalizeUrl,
+//     doesTwoUrlsMatch,
+//     doesNameMatch,
+//     urlPathParts,
+// }
