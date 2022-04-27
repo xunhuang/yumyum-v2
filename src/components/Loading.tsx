9ca8115835c8e6d9dd0ed8557298342954446032
Wilson Huang <xhuang@fb.com>
2022-04-27 16:58:41 -0700

    adding banner to loading page

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,5 +1,22 @@
-import { Spin } from 'antd';
+import { Button, Spin } from 'antd';
 
 export const Loading = () => {
-  return <Spin size="large" />;
+  return (
+    <span>
+      <Spin size="large" />
+      <Button
+        type="primary"
+        size="large"
+        style={{
+          background: "red",
+          borderColor: "red",
+          fontWeight: "bold",
+          marginRight: "5px",
+        }}
+        href="/"
+      >
+        Michelin Reservations Made Easy
+      </Button>
+    </span>
+  );
 };
