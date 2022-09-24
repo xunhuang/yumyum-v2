15cb6c2f801a408aeacdafeef3d57c33a7f5ec4f
Wilson Huang <xhuang@fb.com>
2022-09-24 10:26:52 -0700

    updated authorization token

--- src/yummodule/VendorOpentable.ts
+++ src/yummodule/VendorOpentable.ts
@@ -22,6 +22,7 @@ export class VendorOpentable extends VendorBase {
 
     async venueSearchInternal(venue: VenueVendorInfo, date: string, party_size: number, timeOption: string): Promise<any> {
         // inspired by https://www.vintnersresort.com/dining/john-ash-co/
+        // the authorization token needs to be updated from time to time. 
 
         let url = "https://www.opentable.com/restref/api/availability?lang=en-US";
         let datetime = (timeOption === "dinner") ? date + "T19:00:00" : date + "T12:00:00";
@@ -31,13 +32,12 @@ export class VendorOpentable extends VendorBase {
             "dateTime": datetime,
             "enableFutureAvailability": false
         };
-
         const w = await fetch(url, {
             method: 'post',
             body: JSON.stringify(data),
             headers: {
                 'Content-Type': 'application/json;charset=UTF-8',
-                "authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvdGNmcCI6IjQ0MTM2ZmEzNTViMzY3OGExMTQ2YWQxNmY3ZTg2NDllOTRmYjRmYzIxZmU3N2U4MzEwYzA2MGY2MWNhYWZmOGEiLCJpYXQiOjE2NTU4Nzk1NzMsImV4cCI6MTY1NTg5MDM3M30.RlkrodPw-46Az6_Hv8gE2AHQglbewCvMGmKEW6es550",
+                "authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvdGNmcCI6IjQ0MTM2ZmEzNTViMzY3OGExMTQ2YWQxNmY3ZTg2NDllOTRmYjRmYzIxZmU3N2U4MzEwYzA2MGY2MWNhYWZmOGEiLCJpYXQiOjE2NjQwMzk4MTMsImV4cCI6MTY2NDA1MDYxM30.Kfi_UbwyPsgj-IDewUObsOSItLBLTNDfCqpIHUlDjPU",
             }
         });
 
@@ -104,98 +104,4 @@ export class VendorOpentable extends VendorBase {
             businessid: appconfig[0].rid,
         }
     }
-    /*
-    
-        async fetchVenueInfoFromURL(url) {
-        return await superagent.get(url)
-            .redirects(5)
-            .then((res) => {
-                let venue = {};
-                const $ = cheerio.load(res.text);
-                let scripts = $("script").map(function (i, el) {
-                    let text = cheerio(el).html();
-                    if (text.includes("window.OTDataLayer = ")) {
-                        let aa = text.replace("window.OTDataLayer = ", "").
-                            replace(';', "");
-                        return aa.replace(";", "");
-                    }
-                    return "";
-                }).get().join(' ');
-    
-                let appconfig = JSON.parse(scripts);
-                venue.businessid = appconfig[0].rid;
-    
-                let meta = JSON.parse(cheerio($(".schema-json")[0]).html());
-                venue.name = meta.name;
-                venue.realurl = meta.url;
-                venue.longitude = meta.geo.longitude;
-                venue.latitude = meta.geo.latitude;
-                venue.city = meta.address.addressLocality;
-                venue.region = meta.address.addressRegion;
-                venue.country = meta.address.addressCountry;
-                venue.address = meta.address.streetAddress;
-                venue.zip = meta.address.postalCode;
-    
-                venue.cuisine = meta.servesCuisine;
-                venue.imageList = meta.image;
-                if (meta.image) {
-                    venue.coverImage = meta.image[0];
-                    if (!venue.coverImage) {
-                        venue.coverImage = null;
-                    }
-                }
-                venue.openhours = meta.openingHours;
-                venue.phone = meta.telephone;
-                venue.priceline = meta.priceRange;
-    
-                venue.distinction = "YUMYUM";
-                venue.guide = "YUMYUM";
-                venue.stars = "YUMYUM";
-                venue.close = false;
-                venue.reservation = "opentable";
-                venue.withOnlineReservation = true;
-    
-                venue.localarea = "TBD";
-                return venue;
-            })
-            .catch(err => {
-                console.log("error : " + err);
-                return null;
-            });
-    }
-    
-        async vendor_entity_search(term, longitude, latitude) {
-        let url = "https://www.opentable.com/personalizer-autocomplete/v3/onebox/";
-        return await
-            superagent.get(url)
-                .query({
-                    term: term,
-                    longitude: longitude,
-                    latitude: latitude,
-                })
-                .then((res) => {
-                    return res.body.results.map(item => {
-                        let result = {
-                            businessid: item.id,
-                            name: item.name,
-                        }
-                        if (item.latitude) {
-                            result.latitude = item.latitude;
-                        }
-                        if (item.longitude) {
-                            item.longitude = item.longitude;
-                        }
-                        if (item.region) {
-                            item.city = item.region.name;
-                        }
-                        return result;
-                    })
-                })
-    }
-    
-        async fetchVenueInfoFromSearchItem(item) {
-        let url = `https://www.opentable.com/restaurant/profile/${item.businessid}`;
-        return await this.fetchVenueInfoFromURL(url);
-    }
-    */
 }
