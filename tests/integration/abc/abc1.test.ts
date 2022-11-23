6d019c933c599a310ba75a1900c7a8a07fcef627
Wilson Huang <xhuang@gmail.com>
2022-11-22 20:20:42 -0800

    calling graphql

--- tests/integration/abc/abc1.test.ts
+++ tests/integration/abc/abc1.test.ts
@@ -1,8 +1,35 @@
+import { ApolloClient, gql, InMemoryCache } from '@apollo/client';
 import { describe, expect } from '@jest/globals';
 
 describe('Ingredient DAL', () => {
     beforeAll(async () => {
-        return;
+
+        const cache = new InMemoryCache();
+
+        const client = new ApolloClient({
+            uri:
+                process.env.REACT_APP_GRAPHQL_ENDPOINT ||
+                "https://graph-3khoexoznq-uc.a.run.app/graphql",
+            cache: cache,
+            connectToDevTools: true,
+        });
+        const result = await client.query({
+            query: gql`
+               query LookupReservationInfo($url: String!) {
+  reservationInfo(url: $url) {
+    businessid
+    urlSlug
+    resyCityCode
+    latitude
+    longitude
+    reservation
+  }
+}
+        `, variables: { url: "https://www.exploretock.com/theshotasf/" }
+        });
+        console.log(result);
+
+
         // await dbTeardown() ; ({ id: ingredientId } = await Ingredient.create({
         //         name: 'Beans',
         //         slug: 'beans',
