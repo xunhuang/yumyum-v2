cf2eb7a2e1e408bb1046aba10ed6280f99e8e9b2
Wilson Huang <xhuang@gmail.com>
2023-05-07 12:18:08 -0700

    remove irrelvant files

--- data/Michelin/11-04-2021/run.sh
+++
@@ -1,2 +0,0 @@
-jq -s "map(.results[0] .hits| .[])  " *.json > ../global-11-04-2021.json 
-
--- data/Michelin/go.sh
+++
@@ -1,6 +0,0 @@
-pages=`seq 0 15`
-for p in $pages 
-do 
-   echo "page: " $p
-   sh download.sh $p > page$p.json
-done 
