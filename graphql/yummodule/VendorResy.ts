3dffabb4a77019876d5a82d9b24a10e022af25af
Xun Huang <xhuang@gmail.com>
2024-05-15 15:48:14 -0700

    fix resy

--- graphql/yummodule/VendorResy.ts
+++ graphql/yummodule/VendorResy.ts
@@ -1,16 +1,20 @@
-import { RateLimiter } from 'limiter';
+import { RateLimiter } from "limiter";
 
-import { TimeSlots, VendorBase, VenueReservationInfo, VenueVendorInfo } from './VendorBase';
-import { addressMatch, venueNameMatched } from './venueNameMatched';
-import { VenueSearchInput } from './VenueSearchInput';
+import {
+  TimeSlots,
+  VendorBase,
+  VenueReservationInfo,
+  VenueVendorInfo,
+} from "./VendorBase";
+import { addressMatch, venueNameMatched } from "./venueNameMatched";
+import { VenueSearchInput } from "./VenueSearchInput";
 
-
-const buildUrl = require('build-url');
-const superagent = require('superagent');
-const moment = require('moment-timezone');
-const urlparse = require('url');
+const buildUrl = require("build-url");
+const superagent = require("superagent");
+const moment = require("moment-timezone");
+const urlparse = require("url");
 const getDistance = require("geolib").getDistance;
-const nodefetch = require('node-fetch');
+const nodefetch = require("node-fetch");
 
 const limiter = new RateLimiter({ tokensPerInterval: 5, interval: 1000 }); // 1 request per second;
 
@@ -23,203 +27,248 @@ curl 'https://api.resy.com/3/venue?id=5785' \
   -H 'authorization: ResyAPI api_key="PXnGpHdkz0Y38qg3QdMkRw2GkgBcMEXL"'
 */
 
-
 export class VendorResy extends VendorBase {
+  vendorID() {
+    return "resy";
+  }
 
-    vendorID() {
-        return "resy";
-    }
+  requiedFieldsForReservation() {
+    return ["businessid", "url_slug", "resy_city_code"];
+  }
 
-    requiedFieldsForReservation() {
-        return ["businessid", "url_slug", "resy_city_code"];
-    }
+  async venueSearch(
+    venue: VenueVendorInfo,
+    date: string,
+    party_size: number,
+    timeOption: string
+  ): Promise<TimeSlots[]> {
+    await limiter.removeTokens(1);
+    const url = "https://api.resy.com/4/find";
 
-    async venueSearch(venue: VenueVendorInfo, date: string, party_size: number, timeOption: string): Promise<TimeSlots[]> {
-        await limiter.removeTokens(1);
-        const url = "https://api.resy.com/4/find";
+    // console.log(venue);
+    // let myurl = buildUrl(url, {
+    //     queryParams: {
+    //         day: date,
+    //         lat: 0,
+    //         long: 0,
+    //         party_size: party_size,
+    //         venue_id: venue.businessid,
+    //     }
+    // });
 
-        // console.log(venue);
-        // let myurl = buildUrl(url, {
-        //     queryParams: {
-        //         day: date,
-        //         lat: 0,
-        //         long: 0,
-        //         party_size: party_size,
-        //         venue_id: venue.businessid,
-        //     }
-        // });
+    // const response = await nodefetch(myurl, {
+    //     "agent": new proxyAgent('http://127.0.0.1:8866'),
+    //     headers: {
+    //         "Authorization": 'ResyAPI api_key="VbWk7s3L4KiK5fzlO7JD3Q5EYolJI7n5"',
+    //         "User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+    //         "Accept-Encoding": "identity",
+    //         "accept": "*/*",
+    //     }
+    // });
 
-        // const response = await nodefetch(myurl, {
-        //     "agent": new proxyAgent('http://127.0.0.1:8866'),
-        //     headers: {
-        //         "Authorization": 'ResyAPI api_key="VbWk7s3L4KiK5fzlO7JD3Q5EYolJI7n5"',
-        //         "User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
-        //         "Accept-Encoding": "identity",
-        //         "accept": "*/*",
-        //     }
-        // });
+    // if (response.ok) {
+    //     const data = await response.json();
+    //     console.log(data);
+    // } else {
+    //     console.log(response.status);
+    //     console.log(response);
+    // }
+    // return [];
 
-        // if (response.ok) {
-        //     const data = await response.json();
-        //     console.log(data);
-        // } else {
-        //     console.log(response.status);
-        //     console.log(response);
-        // }
-        // return [];
-
-        return await superagent.get(url)
-            .query({
-                day: date,
-                lat: 0,
-                long: 0,
-                party_size: party_size,
-                venue_id: venue.businessid,
-            })
-            .set('Authorization', 'ResyAPI api_key="VbWk7s3L4KiK5fzlO7JD3Q5EYolJI7n5"')
-            .set('Accept', '*/*')
-            .set("Accept-Encoding", "identity") // resy api does not support gzip/deflate now
-            .set('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36')
-            .then((res: any) => {
-                let total: TimeSlots[] = [];
-                if (!res.body.results.venues) {
-                    return [];
-                }
-                if (!res.body.results.venues[0]) {
-                    return [];
-                }
-                let slots = res.body.results.venues[0].slots;
-                slots.forEach(function (slot: any) {
-                    let datestr =
-                        moment.tz(slot.date.start, venue.timezone).format();
-                    total.push({
-                        time: datestr,
-                    });
-                });
-                return total;
-            });
-    }
-
-    getReservationUrl(venue: VenueVendorInfo, date: string, party_size: number, timeOption: string): string | null {
-        let baseurl = `https://resy.com/cities/${venue.resy_city_code}/${venue.url_slug}`;
-
-        let reservationUrl = buildUrl(baseurl, {
-            queryParams: {
-                date: date,
-                seats: party_size,
-            }
+    return await superagent
+      .get(url)
+      .query({
+        day: date,
+        lat: 0,
+        long: 0,
+        party_size: party_size,
+        venue_id: venue.businessid,
+      })
+      .set(
+        "Authorization",
+        'ResyAPI api_key="VbWk7s3L4KiK5fzlO7JD3Q5EYolJI7n5"'
+      )
+      .set(
+        "User-Agent",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+      )
+      .then((res: any) => {
+        let total: TimeSlots[] = [];
+        if (!res.body.results.venues) {
+          return [];
+        }
+        if (!res.body.results.venues[0]) {
+          return [];
+        }
+        let slots = res.body.results.venues[0].slots;
+        slots.forEach(function (slot: any) {
+          let datestr = moment.tz(slot.date.start, venue.timezone).format();
+          total.push({
+            time: datestr,
+          });
         });
-        return reservationUrl;
+        return total;
+      });
+  }
+
+  getReservationUrl(
+    venue: VenueVendorInfo,
+    date: string,
+    party_size: number,
+    timeOption: string
+  ): string | null {
+    let baseurl = `https://resy.com/cities/${venue.resy_city_code}/${venue.url_slug}`;
+
+    let reservationUrl = buildUrl(baseurl, {
+      queryParams: {
+        date: date,
+        seats: party_size,
+      },
+    });
+    return reservationUrl;
+  }
+
+  async fetchReservationInfoFromURL(
+    url: string
+  ): Promise<VenueReservationInfo | null> {
+    var url_parts = urlparse.parse(url, true);
+    var paths = url_parts.pathname.split("/");
+    let citycode = paths[2];
+    let url_slug = paths[3];
+
+    const venueurl = "https://api.resy.com/3/venue";
+
+    console.log(citycode, url_slug);
+
+    const data = await superagent
+      .get(venueurl)
+      .query({
+        url_slug: url_slug,
+        location: citycode,
+      })
+      .set(
+        "Authorization",
+        'ResyAPI api_key="VbWk7s3L4KiK5fzlO7JD3Q5EYolJI7n5"'
+      )
+      .set(
+        "User-Agent",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
+      )
+      .then((res: any) => {
+        return res.body;
+      });
+    return {
+      reservation: this.vendorID(),
+      businessid: data.id.resy,
+      urlSlug: url_slug,
+      resyCityCode: citycode,
+    };
+  }
+
+  async _APIVenueLookup(slug: string, location: string): Promise<any> {
+    const venueurl = "https://api.resy.com/3/venue";
+    const data = await superagent
+      .get(venueurl)
+      .set(
+        "Authorization",
+        'ResyAPI api_key="VbWk7s3L4KiK5fzlO7JD3Q5EYolJI7n5"'
+      )
+      .query({
+        url_slug: slug,
+        location: location,
+      })
+      .then((res: any) => {
+        return JSON.parse(res.text);
+      });
+    return data?.location;
+  }
+
+  async entitySearchExactTerm(
+    term: string,
+    longitude: number,
+    latitude: number,
+    extra: VenueSearchInput
+  ): Promise<VenueReservationInfo | null> {
+    if (!longitude || !latitude) {
+      console.log(`Resy search API requires long/lat, none provided ${term}`);
+      return null;
     }
 
-    async fetchReservationInfoFromURL(url: string): Promise<VenueReservationInfo | null> {
-        var url_parts = urlparse.parse(url, true);
-        var paths = url_parts.pathname.split("/");
-        let citycode = paths[2];
-        let url_slug = paths[3];
-
-        const venueurl = "https://api.resy.com/3/venue";
-
-        console.log(citycode, url_slug);
-
-        const data = await superagent.get(venueurl)
-            .query({
-                url_slug: url_slug,
-                location: citycode,
-            })
-            .set('Authorization', 'ResyAPI api_key="VbWk7s3L4KiK5fzlO7JD3Q5EYolJI7n5"')
-            .set('User-Agent', "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36")
-            .then((res: any) => {
-                return res.body;
-            });
-        return {
-            reservation: this.vendorID(),
-            businessid: data.id.resy,
-            urlSlug: url_slug,
-            resyCityCode: citycode,
-
+    const url = "https://api.resy.com/3/venuesearch/search";
+    const candidates = await superagent
+      .post(url)
+      .set(
+        "Authorization",
+        'ResyAPI api_key="VbWk7s3L4KiK5fzlO7JD3Q5EYolJI7n5"'
+      )
+      .send({
+        geo: {
+          longitude: longitude,
+          latitude: latitude,
+        },
+        query: term,
+        types: ["venue", "cuisine"],
+      })
+      .then(
+        (res: any) => {
+          let myresult = JSON.parse(res.text);
+          let hits = myresult.search.hits;
+          return hits;
+        },
+        (err: any) => {
+          console.log(err);
+          return [];
         }
+      );
+
+    if (candidates.length === 0) {
+      return null;
     }
 
-    async _APIVenueLookup(slug: string, location: string): Promise<any> {
-        const venueurl = "https://api.resy.com/3/venue";
-        const data = await superagent.get(venueurl)
-            .set('Authorization', 'ResyAPI api_key="VbWk7s3L4KiK5fzlO7JD3Q5EYolJI7n5"')
-            .query({
-                url_slug: slug,
-                location: location,
-            }).then((res: any) => {
-                return JSON.parse(res.text);
-            });
-        return data?.location;
+    const makeResult = (best: any) => {
+      const result = {
+        name: best.name,
+        reservation: this.vendorID(),
+        businessid: `${best.id.resy}`,
+        urlSlug: best.url_slug,
+        resyCityCode: best.location.code,
+      };
+      return result;
+    };
+
+    for (const entry of candidates.slice(0, 10)) {
+      // distance in meters
+      const distance = getDistance(
+        { latitude: latitude, longitude: longitude },
+        { latitude: entry._geoloc.lat, longitude: entry._geoloc.lng }
+      );
+
+      if (distance > 5000) {
+        continue;
+      }
+
+      if (venueNameMatched(term, entry.name)) {
+        return makeResult(entry);
+      }
+
+      const location = await this._APIVenueLookup(
+        entry.url_slug,
+        entry.location.id
+      );
+
+      if (
+        location &&
+        (await addressMatch(
+          location.address_1,
+          extra.address,
+          extra.city,
+          extra.state
+        ))
+      ) {
+        // console.log("address matched", location.address_1, "-", extra.address);
+        return makeResult(entry);
+      }
     }
-
-    async entitySearchExactTerm(term: string, longitude: number, latitude: number, extra: VenueSearchInput): Promise<VenueReservationInfo | null> {
-
-        if (!longitude || !latitude) {
-            console.log(`Resy search API requires long/lat, none provided ${term}`);
-            return null;
-        }
-
-        const url = "https://api.resy.com/3/venuesearch/search";
-        const candidates = await superagent.post(url)
-            .set('Authorization', 'ResyAPI api_key="VbWk7s3L4KiK5fzlO7JD3Q5EYolJI7n5"')
-            .send({
-                geo: {
-                    longitude: longitude,
-                    latitude: latitude,
-                },
-                query: term,
-                types: ["venue", "cuisine"],
-            })
-            .then((res: any) => {
-                let myresult = JSON.parse(res.text);
-                let hits = myresult.search.hits;
-                return hits;
-            }, (err: any) => {
-                console.log(err);
-                return [];
-            });
-
-        if (candidates.length === 0) {
-            return null;
-        }
-
-        const makeResult = (best: any) => {
-
-            const result = {
-                name: best.name,
-                reservation: this.vendorID(),
-                businessid: `${best.id.resy}`,
-                urlSlug: best.url_slug,
-                resyCityCode: best.location.code,
-            };
-            return result;
-        }
-
-        for (const entry of candidates.slice(0, 10)) {
-            // distance in meters
-            const distance = getDistance(
-                { latitude: latitude, longitude: longitude },
-                { latitude: entry._geoloc.lat, longitude: entry._geoloc.lng }
-            );
-
-            if (distance > 5000) {
-                continue;
-            }
-
-            if (venueNameMatched(term, entry.name)) {
-                return makeResult(entry);
-            }
-
-            const location = await this._APIVenueLookup(entry.url_slug, entry.location.id);
-
-            if (location && await addressMatch(location.address_1, extra.address, extra.city, extra.state)) {
-
-                // console.log("address matched", location.address_1, "-", extra.address);
-                return makeResult(entry);
-            }
-        }
-        return null;
-    }
+    return null;
+  }
 }
--- tests/resy.test.ts
+++ tests/resy.test.ts
@@ -1,73 +1,89 @@
-import { describe, expect } from '@jest/globals';
+import { describe, expect } from "@jest/globals";
 
-import { VendorResy } from '../graphql/yummodule/VendorResy';
-import { VenueSearchInput } from '../graphql/yummodule/VenueSearchInput';
-import { venueReservationSearchByKey } from './getYumYumGraphQLClient';
+import { VendorResy } from "../graphql/yummodule/VendorResy";
+import { VenueSearchInput } from "../graphql/yummodule/VenueSearchInput";
+import { venueReservationSearchByKey } from "./getYumYumGraphQLClient";
 
 const nyc = require("./nyc-tbd.json");
 
 var resy = new VendorResy();
-describe('Resy System Test', () => {
-    const AurumData: VenueSearchInput = {
-        "name": "Aurum",
-        "latitude": 37.3801255,
-        "longitude": -122.1156105,
-        "address": "132 State St",
-        "city": "Los Altos",
-        "state": "CA"
-    }
+describe("Resy System Test", () => {
+  const HeirloomData: VenueSearchInput = {
+    name: "Heirloom Cafe",
+    city: "San Francisco",
+    address: "2500 Folsom St",
+    latitude: 37.757116,
+    longitude: -122.414713,
+    state: "CA",
+  };
 
-    beforeAll(async () => {
-        return;
-    })
+  beforeAll(async () => {
+    return;
+  });
 
-    afterAll(async () => {
-        // await dbTeardown()
-    })
+  afterAll(async () => {
+    // await dbTeardown()
+  });
 
-    describe('Search entity by name and long/lat', () => {
-        it('should find an entity with exact match', async () => {
-            const search_result = await resy.entitySearchExactTerm("Aurum", -122.1156105, 37.3801255, AurumData);
-            expect(search_result).not.toBeNull();
-            expect(search_result!.businessid).toEqual("49088");
-            expect(search_result!.urlSlug).toEqual("aurum");
-            expect(search_result!.resyCityCode).toEqual("lsl");
-        })
-
-        it('should not find an entity with complete garbage', async () => {
-            const search_result = await resy.entitySearchExactTerm("adadadfsfsdf2weweweAurum", -122.1156105, 37.3801255, AurumData);
-            expect(search_result).toBeNull()
-        })
-
-        // disableing these two tests. becuase we are doing exact matchings now, validating
-        // the name or address are needed.
-        // it('should find an entity with fuzzzy match', async () => {
-        //     const search_result = await resy.entitySearchExactTerm("Au", -122.1156105, 37.3801255, AurumData);
-        //     expect(search_result.urlSlug).toEqual("aurum");
-        // })
-
-        // it('should find an entity with fuzzzy match, case not match', async () => {
-        //     const search_result = await resy.entitySearchExactTerm("au", -122.1156105, 37.3801255, AurumData);
-        //     expect(search_result.urlSlug).toEqual("aurum");
-        // })
-
-        it('list of entity (like fail because 5 second limit)', async () => {
-            for (const entity of nyc) {
-                const search_result = await resy.entitySearchExactTerm(entity.name, entity.longitude, entity.latitude, AurumData);
-                if (search_result) {
-                    console.log(entity.name, search_result)
-                }
-            }
-        }, 100000)
-    })
-
-    describe('reservation search', () => {
-        it('Testing for Resy API functioning ok', async () => {
-            // ASA South
-            const search_result = await venueReservationSearchByKey("00d065a4-8869-4ade-86fc-06e37bc918e6");
-            // null means API error
-            expect(search_result).not.toBeNull();
-        })
+  describe("Search entity by name and long/lat", () => {
+    it("should find an entity with exact match", async () => {
+      const search_result = await resy.entitySearchExactTerm(
+        "heirloom",
+        -122.414713,
+        37.757116,
+        HeirloomData
+      );
+      expect(search_result).not.toBeNull();
+      expect(search_result!.businessid).toEqual("7074");
+      expect(search_result!.urlSlug).toEqual("heirloom-cafe");
+      expect(search_result!.resyCityCode).toEqual("sf");
     });
-})
 
+    it("should not find an entity with complete garbage", async () => {
+      const search_result = await resy.entitySearchExactTerm(
+        "adadadfsfsdf2weweweAurum",
+        -122.1156105,
+        37.3801255,
+        HeirloomData
+      );
+      expect(search_result).toBeNull();
+    });
+
+    // disableing these two tests. becuase we are doing exact matchings now, validating
+    // the name or address are needed.
+    // it('should find an entity with fuzzzy match', async () => {
+    //     const search_result = await resy.entitySearchExactTerm("Au", -122.1156105, 37.3801255, AurumData);
+    //     expect(search_result.urlSlug).toEqual("aurum");
+    // })
+
+    // it('should find an entity with fuzzzy match, case not match', async () => {
+    //     const search_result = await resy.entitySearchExactTerm("au", -122.1156105, 37.3801255, AurumData);
+    //     expect(search_result.urlSlug).toEqual("aurum");
+    // })
+
+    it("list of entity (like fail because 5 second limit)", async () => {
+      for (const entity of nyc) {
+        const search_result = await resy.entitySearchExactTerm(
+          entity.name,
+          entity.longitude,
+          entity.latitude,
+          HeirloomData
+        );
+        if (search_result) {
+          console.log(entity.name, search_result);
+        }
+      }
+    }, 100000);
+  });
+
+  describe("reservation search", () => {
+    it("Testing for Resy API functioning ok", async () => {
+      // Heirloom Cafe
+      const search_result = await venueReservationSearchByKey(
+        "YUNn5L0CP1HjttnY7NU6"
+      );
+      // null means API error
+      expect(search_result).not.toBeNull();
+    });
+  });
+});
