579aed89811f77598827afc3b1265538a2c37aa2
Xun Huang <xhuang@gmail.com>
2024-05-26 16:33:25 -0700

    fix some bugs

--- cloudfunctions/findResyReservation.js
+++ cloudfunctions/findResyReservation.js
@@ -1,44 +1,26 @@
 const dayjs = require("dayjs");
-const superagent = require("superagent");
+const { newFindReservation } = require("../tockspecial/resy_support");
 
-async function findResyReservation(date, party_size, businessid, timezone, url_slug) {
-  const url = "https://api.resy.com/4/find";
-
-  return await superagent
-    .get(url)
-    .query({
-      day: date,
-      lat: 0,
-      long: 0,
-      party_size: party_size,
-      venue_id: businessid,
-    })
-    .set("Accept-Encoding", "identity")
-    .set("Authorization", 'ResyAPI api_key="VbWk7s3L4KiK5fzlO7JD3Q5EYolJI7n5"')
-    .set(
-      "User-Agent",
-      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
-    )
-    .then((res) => {
-      console.log(res.body);
-      let total = [];
-      if (!res.body.results.venues) {
-        return [];
-      }
-      if (!res.body.results.venues[0]) {
-        return [];
-      }
-      let slots = res.body.results.venues[0].slots;
-      slots.forEach(function (slot) {
-        let datestr = dayjs.tz(slot.date.start, timezone).format();
-        total.push({
-          time: datestr,
-        });
-      });
-      return total;
-    })
-    .catch((err) => {
-      console.log(err);
+async function findResyReservation(
+  date,
+  party_size,
+  businessid,
+  timezone,
+  url_slug
+) {
+  const slots = await newFindReservation(businessid, date, party_size);
+  const total = [];
+  if (slots === null || !Array.isArray(slots)) {
+    console.log("slots is null not not an array", slots);
+    return null;
+  }
+  slots.forEach(function (slot) {
+    let datestr = dayjs.tz(slot.date.start, timezone).format();
+    total.push({
+      time: datestr,
     });
+  });
+  return total;
 }
+
 exports.findResyReservation = findResyReservation;
--- cloudfunctions/index.js
+++ cloudfunctions/index.js
@@ -26,17 +26,40 @@ functions.http("resy_reservation_find", async (req, res) => {
   return res.send(JSON.stringify(result));
 });
 
-functions.http("resy_reservation_find_redis", async (req, res) => {
-  const key = resy_day_key(
-    req.query.url_slug,
+functions.http("resy_reservation_find_realtime", async (req, res) => {
+  const result = await findResyReservation(
     req.query.date,
     req.query.party_size,
+    req.query.businessid,
+    req.query.timezone,
+    req.query.url_slug,
   );
-  console.log("key", key);
 
+  return res.send(JSON.stringify(result));
+});
+
+functions.http("resy_reservation_find_redis", async (req, res) => {
+  const total = await resy_redis(req.query.url_slug, req.query.date, req.query.party_size, req.query.timezone);
+  return res.send(JSON.stringify(total));
+});
+
+// This function is used to get the available slots from Redis
+// if slots are available in Redis, it will return the slots in json format
+// [{ time: "2022-02-24T21:00:00-08:00" }, { time: "2022-02-24T21:15:00-08:00" }]
+// If no slot are available in Redis, it will return an empty array
+// If the business it not legit, it should return return null
+async function resy_redis(
+  url_slug, date, party_size, time_zone) {
+  const key = resy_day_key(
+    url_slug,
+    date,
+    party_size
+  );
   const slots = await redis.get(key);
-  const timezone = req.query.timezone;
-
+  if (!slots) {
+    return null;
+  }
+  const timezone = time_zone;
   const total = [];
   slots.forEach(function (slot) {
     let datestr = dayjs.tz(slot, timezone).format();
@@ -44,6 +67,6 @@ functions.http("resy_reservation_find_redis", async (req, res) => {
       time: datestr,
     });
   });
+  return total;
+}
 
-  return res.send(JSON.stringify(total));
-});
