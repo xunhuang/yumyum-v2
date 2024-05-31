9a600ab3aac1620d93dc474b11cabbc5c282e86f
Xun Huang <xhuang@gmail.com>
2024-05-30 19:01:10 -0700

    opentable scan seems to work

--- tockspecial/reservation-finder.js
+++ tockspecial/reservation-finder.js
@@ -1,50 +1,17 @@
-const buildUrl = require("build-url");
 const cheerio = require("cheerio");
 
-const { saveToRedisWithChunking } = require("./saveToRedisWithChunking");
-const { RateLimiter } = require("limiter");
-const limiter = new RateLimiter({ tokensPerInterval: 1, interval: 1000 });
-
-const dayjs = require("dayjs");
-const { resy_calendar_key } = require("./resy_support");
-const { resyLists } = require("./resy_support");
 const { yumyumGraphQLCall } = require("./yumyumGraphQLCall");
+const { opentable_set_venue_to_tbd } = require("./resy_support");
 
 (async function main() {
   try {
     const bayAreaList = await BayAreaListWithTBD();
     for (let v of bayAreaList) {
-      console.log(v);
-      const content = await fetch(
-        v.url,
-        // const content = await fetch(
-        //   "https://guide.michelin.com/us/en/california/calistoga/restaurant/solbar",
-        {
-          headers: {
-            accept:
-              "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-            "accept-language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7",
-            priority: "u=0, i",
-            "sec-ch-ua":
-              '"Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"',
-            "sec-ch-ua-mobile": "?0",
-            "sec-ch-ua-platform": '"macOS"',
-            "sec-fetch-dest": "document",
-            "sec-fetch-mode": "navigate",
-            "sec-fetch-site": "none",
-            "sec-fetch-user": "?1",
-            "upgrade-insecure-requests": "1",
-          },
-          referrerPolicy: "strict-origin-when-cross-origin",
-          body: null,
-          method: "GET",
-        }
-      );
-
-      const body = await content.text();
+      const body = await simpleFetchGet(v.url);
       const $ = cheerio.load(body);
 
       var reservation_links = [];
+      // eslint-disable-next-line no-loop-func
       $('a[data-event="partner_book"]').each((index, element) => {
         console.log(element.attribs["href"]);
         reservation_links.push(element.attribs["href"]);
@@ -63,12 +30,56 @@ const { yumyumGraphQLCall } = require("./yumyumGraphQLCall");
             console.log("resy: venueId not found");
           }
         });
+      } else {
+        if (reservation_links[0].includes("opentable")) {
+          const opentablelink = reservation_links[0];
+          const opentablewebsite = await simpleFetchGet(opentablelink);
+          const $ = cheerio.load(opentablewebsite);
+          // Extract the restaurantId from the 'al:ios:url' meta tag
+          const iosUrlContent = $('meta[property="al:ios:url"]').attr('content');
+          const restaurantId = iosUrlContent.match(/rid=(\d+)/)[1];
+
+          console.log(v);
+          console.log('Restaurant ID:', restaurantId, v.key);
+          await opentable_set_venue_to_tbd(v.key, restaurantId);
+          let url = `https://www.opentable.com/restaurant/profile/${restaurantId}/reserve`;
+          console.log(url);
+        }
+
       }
+      break;
     }
   } catch (error) {
     console.error(error);
   }
 })();
+
+async function simpleFetchGet(url) {
+  const content = await fetch(
+    url,
+    {
+      headers: {
+        accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "accept-language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7",
+        priority: "u=0, i",
+        "sec-ch-ua": '"Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"macOS"',
+        "sec-fetch-dest": "document",
+        "sec-fetch-mode": "navigate",
+        "sec-fetch-site": "none",
+        "sec-fetch-user": "?1",
+        "upgrade-insecure-requests": "1",
+      },
+      referrerPolicy: "strict-origin-when-cross-origin",
+      body: null,
+      method: "GET",
+    }
+  );
+
+  const body = await content.text();
+  return body;
+}
 
 async function BayAreaListWithTBD() {
   const query = `
--- tockspecial/resy_support.js
+++ tockspecial/resy_support.js
@@ -89,3 +89,23 @@ mutation MyMutation {
   const json = await yumyumGraphQLCall(query);
   return json;
 }
+
+exports.opentable_set_venue_to_tbd = async function (venue_key, businessid) {
+  const query = `
+mutation MyMutation {
+  updateVenueByKey(input: {venuePatch: {
+    reservation: "opentable",
+    businessid: "${businessid}",
+  }, key: "${venue_key}"}) {
+  venue {
+    name
+    key
+    closehours
+  }
+  }
+}
+`;
+
+  const json = await yumyumGraphQLCall(query);
+  return json;
+}
