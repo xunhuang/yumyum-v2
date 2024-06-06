27a23c4d228eae016dba4e187cfc0a408d6c98c0
Xun Huang <xhuang@gmail.com>
2024-06-06 15:37:53 -0700

    adding user agent for resy search

--- tockspecial/resy_support.js
+++ tockspecial/resy_support.js
@@ -32,7 +32,7 @@ nodes {
 exports.resyLists = resyLists;
 async function newFindReservation(venue_id, date, party_size) {
   await limiter.removeTokens(1);
-  const a = await fetch(
+  const result = await resyAPIFetch(
     buildUrl("https://api.resy.com", {
       path: "4/find",
       queryParams: {
@@ -42,32 +42,8 @@ async function newFindReservation(venue_id, date, party_size) {
         party_size: party_size,
         venue_id: venue_id,
       },
-    }),
-    {
-      headers: {
-        accept: "application/json, text/plain, */*",
-        "accept-language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7",
-        authorization: 'ResyAPI api_key="VbWk7s3L4KiK5fzlO7JD3Q5EYolJI7n5"',
-        "cache-control": "no-cache",
-        priority: "u=1, i",
-        "sec-ch-ua":
-          '"Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"',
-        "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": '"macOS"',
-        "sec-fetch-dest": "empty",
-        "sec-fetch-mode": "cors",
-        "sec-fetch-site": "same-site",
-        "x-origin": "https://resy.com",
-      },
-      referrer: "https://resy.com/",
-      referrerPolicy: "strict-origin-when-cross-origin",
-      body: null,
-      method: "GET",
-      mode: "cors",
-      credentials: "include",
-    }
-  );
-  return await a.json();
+    }));
+  return result;
 }
 exports.newFindReservation = newFindReservation;
 function resy_day_key(slug, date, party_size) {
@@ -341,6 +317,8 @@ async function resy_basic_search(term, longitude, latitude) {
         "x-origin": "https://resy.com",
         Referer: "https://resy.com/",
         "Referrer-Policy": "strict-origin-when-cross-origin",
+        "user-agent":
+          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
       },
       body: JSON.stringify({
         geo: {
