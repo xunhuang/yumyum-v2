f6556ebb59956fe027be537bbab28f6afcd0e52a
Xun Huang <xhuang@gmail.com>
2024-06-14 19:58:50 -0700

    get module test

---
+++ yumutil/test/yumyumGraphQLCall.test.ts
@@ -0,0 +1,27 @@
+import { describe, expect } from "@jest/globals";
+import { yumyumGraphQLCall } from "yumutil";
+
+describe("yumyum graphql calls", () => {
+  it("basic graphql call for bay area", async () => {
+    const query = `
+    query MyQuery {
+      allVenues(
+        filter: {
+          metro: { equalTo: "bayarea" }
+          close: { equalTo: false }
+        }
+      ) {
+        totalCount
+        nodes {
+          name
+          urlSlug
+          reservation
+        }
+      }
+    }`;
+    const result = await yumyumGraphQLCall(query);
+    console.log("hihi -----------", result.data.allVenues.totalCount);
+    expect(result.data.allVenues.totalCount).toBeGreaterThan(10);
+    expect(result.data.allVenues.nodes).not.toBeNull();
+  });
+});
