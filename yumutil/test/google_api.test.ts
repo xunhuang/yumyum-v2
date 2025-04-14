9574937910b0d5826bc46f158de9aea4f0bc87de
Xun Huang <xhuang@gmail.com>
2025-04-14 10:45:03 -0700

    google api unit test

--- yumutil/test/google_api.test.ts
+++ yumutil/test/google_api.test.ts
@@ -19,4 +19,10 @@ describe("Google API test", () => {
     expect(result).toContain("2213 S Shore Center");
   });
 
+  it("google_find_street_address_from_text_2", async () => {
+    const result = await GoogleFindStreetAddressFromText("Seventh & Dolores, Carmel-by-the-Sea, CA");
+    expect(result).not.toBeNull();
+    expect(result).toContain("2213 S Shore Center");
+  });
+
 });
